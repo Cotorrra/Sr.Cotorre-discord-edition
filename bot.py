57d0c2fb865049753bc2824645a45537f6ec28cf
Joaquín Pérez <joaquinpereza96@gmail.com>
2021-02-08 09:04:22 -0300

    Updated Help

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ from utils import *
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='!ah')
+bot = commands.Bot(command_prefix='!')
+
 
 ah_all_cards = requests.get('https://es.arkhamdb.com/api/public/cards?encounter=1').json()
 
@@ -17,26 +18,35 @@ ah_player = requests.get('https://es.arkhamdb.com/api/public/cards?encounter=0')
 # Encounter cards include: Special player cards, Weaknesses, enemies, acts, plans, etc.
 ah_encounter = [c for c in ah_all_cards if "spoiler" in c]
 
-showing = False
+raw_text = False
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} está listo para usarse c:')
+    await bot.change_presence(activity=discord.Game(name="a mejorar mazos"))
 
 
 # @bot.command(name='t', help='Busca el registro de tabú de la carta pedida')
 # async def look_for_taboo(ctx):
 
-@bot.command(name='help')
+@bot.command(name='ahhelp')
 async def send_help(ctx):
-    response = "!ahd [numero] Busca en ArkhamDB el mazo dado y lo muestra, tanto públicos como privados.\n" \
-                "!ahj [nombre] ~[subtitulo]~ ([extra]) Busca cartas en ArkhamDB.\n" \
-                "[extra] puede contener ser lo siguiente: '0-5' nivel de la carta, " \
-                "'G/B/R/M/S/N' la clase de la carta, P para permanente, U para único, E para excepcional."
-    ctx.send(response)
+    res = "¿Necesitas ayuda?: \n" \
+          "" \
+          "\n- !ahj [nombre] ~[subtitulo]~ ([extra]): Busca cartas en ArkhamDB.\n" \
+          "[extra] puede contener ser lo siguiente: '0-5' nivel de la carta, " \
+          "'G/B/R/M/S/N' la clase de la carta, P para permanente, U para único, E para excepcional.\n" \
+          "Por ejemplo: \"!ahj Whisky (3S)\" devolverá el Whisky de Mosto Ácido de Supervivente de nivel 3. \n" \
+          "\n- !ahm [nombre] ~[subtitulo]~: Busca cartas de encuentros (lugares, actos, escenarios, etc.) que no " \
+          "sean cartas de jugador estándar. (¡Spoilers!) \n" \
+          "\n- !ahd [numero]: Busca en ArkhamDB el mazo dado y lo muestra, tanto público como privado.\n" \
+          "\n- !ahu [numero] [numero] Busca en ArkhamDB ambos mazos y muestra las mejoras realizadas en los mazos." \
+          "Si mejoraste el mazo con ArkhamDB puedes también entregarle sólo el número del mazo más reciente." \
+          ""
+    await ctx.send(res)
 
 
-@bot.command(name='j')
+@bot.command(name='ahj')
 async def look_for_player_card(ctx):
     query = ' '.join(ctx.message.content.split()[1:])
     query, keyword_query, keyword_mode = find_and_extract(query, "(", ")")
@@ -70,25 +80,21 @@ async def look_for_player_card(ctx):
     await dev_send(showing, ctx, response)
 
 
-@bot.command(name='d')
+@bot.command(name='ahd')
 async def look_for_deck(ctx, code: str):
-    link = 'https://es.arkhamdb.com/api/public/deck/%s' % code
-    req = requests.get(link)
-    if req.url != link:
-        link = 'https://es.arkhamdb.com/api/public/decklist/%s' % code
-        req = requests.get(link)
-        if req.url != link:
-            response = "Mazo no encontrado"
-            await dev_send(showing, ctx, response)
+    deck = find_deck(code)
+    if not deck:
+        response = "Mazo no encontrado"
+        await ctx.send(response)
+    else:
+        deck_info = format_deck_cards(deck, ah_all_cards)
+        embed = format_deck(deck, deck_info)
+        response = "¡Mazo Encontrado!"
+        await ctx.send(response, embed=embed)
 
-    deck_info = format_deck_cards(req.json(), ah_all_cards)
-    response = format_deck(req.json(), deck_info)
 
-    await dev_send(showing, ctx, response)
-
-""" # TODO: Armar los format_x...+
-@bot.command(name='m')
-async def look_for_encounter(ctx, code: str):
+@bot.command(name='hm')
+async def look_for_encounter(ctx):
     query = ' '.join(ctx.message.content.split()[1:])
     query, keyword_query, keyword_mode = find_and_extract(query, "(", ")")
     query, sub_query, sub_text_mode = find_and_extract(query, "~", "~")
@@ -131,5 +137,6 @@ async def dev_send(debug, ctx, string):
         await ctx.send("```%s```" % string)
     else:
         await ctx.send(string)
+
 
 bot.run(TOKEN)
