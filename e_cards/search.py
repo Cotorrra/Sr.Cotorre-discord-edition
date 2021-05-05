ba18c36048f495607f323b065d95cccc684a9da2
Joaquín Pérez <joaquinpereza96@gmail.com>
2021-05-05 02:21:02 -0400

    Added the Dunwich Deluxe FAQ

--- e_cards/search.py
+++ e_cards/search.py
@@ -34,7 +34,8 @@ def use_ec_keywords(cards: list, key_list: str):
 
 def format_query_ec(kwargs):
     name = kwargs.get('nombre')
-    subtitle = f" ~{kwargs.get('subtitulo')}~" if kwargs.get('subtitulo') else " "
-    tipo = kwargs.get('tipo') if kwargs.get('clase') else ""
+    subtitle = f" ~{kwargs.get('subtitulo')}~" if kwargs.get('subtitulo') else ""
+    tipo = kwargs.get('tipo') if kwargs.get('tipo') else ""
     extra = f" ({tipo})"
+    print(name + subtitle + extra)
     return name + subtitle + extra
