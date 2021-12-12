feaf1de318bfe405f0de09ec3fc10db777a74d56
Cotorra <theafterburner750@gmail.com>
2021-12-12 18:14:50 -0300

    Updated components

--- src/response/components.py
+++ src/response/components.py
@@ -7,16 +7,16 @@ from src.core.translator import locale
 buttons = [
             create_button(
                 style=ButtonStyle.red,
-                label=locale('self_destruct')
+                label=locale('self_delete')
             ),
           ]
 action_row = create_actionrow(*buttons)
 
 
 async def self_destruct(bot, ctx, user_id):
-    button_ctx: ComponentContext = await wait_for_component(bot, components=action_row)
-    if user_id == button_ctx.author_id:
-        await ctx.message.delete()
-    else:
-        await ctx.send("<:confusedwatermelon:739425223358545952>")
-
+    message_deleted = False
+    while not message_deleted:
+        button_ctx: ComponentContext = await wait_for_component(bot, components=action_row)
+        if user_id == button_ctx.author_id:
+            message_deleted = True
+            await ctx.message.delete()
