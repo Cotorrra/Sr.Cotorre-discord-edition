f12f45ec55c20d51f067caa4997b8cef102206e5
Joaquín Pérez <theafterburner750@gmail.com>
2022-07-13 20:36:11 -0400

    Updated ah_player cards filter

--- src/core/cardsDB.py
+++ src/core/cardsDB.py
@@ -10,7 +10,7 @@ class CardsDB:
     def __init__(self):
         self.ah_all_cards = requests.get(f'{ARKHAM_DB}/api/public/cards?encounter=1').json()
         self.ah_player = requests.get(f'{ARKHAM_DB}/api/public/cards?encounter=0').json()
-        self.ah_player = [c for c in self.ah_player if "duplicate_of_code" not in c]
+        self.ah_player = [c for c in self.ah_player if "duplicate_of_code" not in c and not c['faction_code'] == 'mythos']
         self.ah_encounter = [c for c in self.ah_all_cards if "spoiler" in c]
 
     def get_all_cards(self):
